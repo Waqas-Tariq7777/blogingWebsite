fae6c429d912a983c36d6e6664781212acd51ec3
Waqas-Tariq7777 <77waqastariq77@gmail.com>
2025-09-22 12:44:59 +0500

    nothing1

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,15 +2,9 @@ import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-dotenv.config({ path: "./.env"})
-const port = process.env.PORT || 4000
-connectDB()
-.then(()=>{
-app.listen(port, (req, res)=>{
-    console.log("App is Successfully listening on Port:", port)
-})
-})
-.catch((error)=>{
-    console.log("MONGODB Connection Error:", error)
-})
+dotenv.config();
 
+await connectDB();
+
+// export app for Vercel
+export default app;
